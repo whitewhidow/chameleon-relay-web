991c173a91941dc8c5ae29971d09d73349b586a7
whitewhidow <contact@branchbit.be>
2026-09-08 21:55:01 +0200

    portal: stamp BUILD tag rrp-split+sniff so stale-cache is visible (build 20260908sniff2)

--- relay.js
+++ relay.js
@@ -18,7 +18,7 @@
  */
 'use strict';
 
-const BUILD = '2026-09-09h leds';   // shown in the log so you can confirm which version loaded
+const BUILD = '20260908 rrp-split+sniff';   // shown in the log so you can confirm which version loaded
 
 // --- Nordic UART Service (verified in firmware ble_main.c / ble_nus) ---------
 const NUS_SERVICE = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
